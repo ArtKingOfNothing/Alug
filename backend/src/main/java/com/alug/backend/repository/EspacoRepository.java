6514826249f990aeea674562abe19ab91f36b141
Artur Francisco de Paiva Batista <artur.batista@estudante.ufla.br>
2026-06-22 17:36:16 -0300

    feat: mudanças para implementar crud de espaco 2

--- backend/src/main/java/com/alug/backend/repository/EspacoRepository.java
+++ backend/src/main/java/com/alug/backend/repository/EspacoRepository.java
@@ -4,6 +4,10 @@ import com.alug.backend.model.Espaco;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EspacoRepository extends JpaRepository<Espaco, Integer> {
+    // RF-020
+    Optional<Espaco> findByEndereco_LogradouroContainingIgnoreCase(String logradouro);
 }
