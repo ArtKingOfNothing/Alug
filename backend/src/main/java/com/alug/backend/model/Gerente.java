00083899c935f5fa30a0f3d86ef58c0de59e27d5
Artur Francisco de Paiva Batista <artur.batista@estudante.ufla.br>
2026-06-22 14:45:05 -0300

    feat: mudança em gerente e no pom para adicionar login posteriormente

--- backend/src/main/java/com/alug/backend/model/Gerente.java
+++ backend/src/main/java/com/alug/backend/model/Gerente.java
@@ -5,11 +5,17 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.br.CPF;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @Table(name = "gerentes")
-public class Gerente {
+public class Gerente implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,4 +52,45 @@ public class Gerente {
     @JoinColumn(name = "enderecos_id_endereco", nullable = false)
     private Endereco endereco;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() { // super admin tem tudo que gerente tem + plus
+        if (this.perfil == PerfilAcesso.SUPER_ADMIN) {
+            return List.of(
+                    new SimpleGrantedAuthority("ROLE_SUPER_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_GERENTE_COMUM")
+            );
+        } else {
+            return List.of(new SimpleGrantedAuthority("ROLE_GERENTE_COMUM"));
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
